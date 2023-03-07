f786a54e44e42f7d5dba81fcb6113670dd6f1f4a
Albin Binu <114827093+Binuchii@users.noreply.github.com>
2023-03-07 12:24:28 +0000

    Solve Round 1

--- challenge1/command.py
+++ challenge1/command.py
@@ -6,4 +6,13 @@ def hello(message):
     # Your job is to process the message so that this function returns the correct outputs for challenge 1.
     # (for now, it just echoes back the same message)
 
-    return message
+    user_input = input("Enter something, could be your name: ")
+    print(user_input)
+    message_length = len(user_input)
+
+    if message_length == 0:
+        message = "Hello, Cisco"
+        return message
+    else:
+        message = 'Hello, ' + user_input + '!'
+        return message
